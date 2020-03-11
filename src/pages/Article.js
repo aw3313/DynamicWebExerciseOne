a87fe520bd0a1ccf228d1884f93b3442eeb48102
aw3313 <aw3313@nyu.edu>
2020-03-11 10:31:21 -0400

    second commit

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -6,14 +6,42 @@ import Data from '../components/Data';
 
 function Article() {
   let {id} = useParams();
+  const [article,setArticle] = useState({});
+
+  useEffect(() =>{
+    let dataArray = Data.filter((article) => article.id == id);
+    setArticle(dataArray[0]);
+  }, [id])
+
+  let d = article && article.publishedDate;
+
 
   return (
-  <div>
-    <header>
-      <h1>Article</h1>
-    </header>
-    <main>
-      <Article/>
+  <div className="Article">
+
+    <header
+      className="articleHeader"
+      style={{}}>
+        <div className="articleHeaderWrapper">
+          <h1> {article.title}</h1>
+          <p> {article.publishedDate && new Intl.DateTimeFormat('en-US',
+          {
+            weekday:"long",
+            month: "long",
+            day: "2-digit",
+            year: "numeric"
+
+          }
+        ).format(new Date(article.publishedDate))}</p>
+        <p class="articleBlurb">{article.blurb}</p>
+        </div>
+        </header>
+
+
+    <main className="articleContent">
+      <div className="articleContentWrapper">
+        {article.articleText && article.articleText.map((text,i)=>(<text.type key={i}>{text.data}</text.type>))}
+      </div>
     </main>
   </div>
 );
